d68c3e67686002d0f26e2e9ca4268442cf8d826c
leenakf <leenakhaled78@gmail.com>
2025-11-09 18:49:52 +0300

    Update add-question-action.php

--- Masar_G2/add-question-action.php
+++ Masar_G2/add-question-action.php
@@ -2,18 +2,18 @@
 session_start();
 require 'connection.php';
 
-if (!isset($_SESSION['user_id']) || (isset($_SESSION['user_type']) && $_SESSION['user_type'] !== 'educator')) {
-  header("Location: index.php");
+if (!isset($_SESSION['id']) || $_SESSION['userType'] !== 'educator') {
+  header("Location: login.php");
   exit;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['__add_question'])) {
-  $qtext   = mysqli_real_escape_string($conn, $_POST['qtext']);
-  $optA    = mysqli_real_escape_string($conn, $_POST['optA']);
-  $optB    = mysqli_real_escape_string($conn, $_POST['optB']);
-  $optC    = mysqli_real_escape_string($conn, $_POST['optC']);
-  $optD    = mysqli_real_escape_string($conn, $_POST['optD']);
-  $correct = mysqli_real_escape_string($conn, $_POST['correct']);
+  $qtext   = mysqli_real_escape_string($connection, $_POST['qtext']);
+  $optA    = mysqli_real_escape_string($connection, $_POST['optA']);
+  $optB    = mysqli_real_escape_string($connection, $_POST['optB']);
+  $optC    = mysqli_real_escape_string($connection, $_POST['optC']);
+  $optD    = mysqli_real_escape_string($connection, $_POST['optD']);
+  $correct = mysqli_real_escape_string($connection, $_POST['correct']);
   $quizID  = (int)$_POST['quizID'];
 
   // رفع صورة (اختياري)
@@ -27,15 +27,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['__add_question'])) {
   }
 
   // إدراج في DB (اسم الجدول بالحروف الصغيرة كما في الـ SQL)
-  $sql = "INSERT INTO quizquestion
-          (quizID, question, answerA, answerB, answerC, answerD, correctAnswer, questionFigureFileName)
-          VALUES ($quizID, '$qtext', '$optA', '$optB', '$optC', '$optD', '$correct', " . ($imageName ? "'$imageName'" : "NULL") . ")";
-
-  if (!mysqli_query($conn, $sql)) {
-    die('خطأ في الإدراج: ' . mysqli_error($conn));
+ $sql = "INSERT INTO quizquestion
+        (quizID, question, answerA, answerB, answerC, answerD, correctAnswer, questionFigureFileName)
+        VALUES ($quizID, '$qtext', '$optA', '$optB', '$optC', '$optD', '$correct', " . 
+        ($imageName ? "'$imageName'" : "NULL") . ")";
+  if (!mysqli_query($connection, $sql)) {
+    die('خطأ في الإدراج: ' . mysqli_error($connection));
   }
 
-  header('Location: quiz.php?quizID=' . $quizID);
+  header('Location: Quiz.php?quizID=' . $quizID);
   exit;
 }
 
